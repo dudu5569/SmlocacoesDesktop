23065d6042382187b030123207326f1af151ee60
EDUARDO RICHARD LEITE SANTOS <eduardo.rlsantos@senacsp.edu.br>
2024-09-17 09:07:45 -0300

    Criação dos formulários

--- SmLocaçõesLib/Cliente.cs
+++ SmLocaçõesLib/Cliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmLocaçõesLib
 {
-    internal class Cliente
+    public class Cliente
     {
     }
 }
--- SmLocaçõesLib/Estoque.cs
+++ SmLocaçõesLib/Estoque.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmLocaçõesLib
 {
-    internal class Estoque
+    public class Estoque
     {
     }
 }
