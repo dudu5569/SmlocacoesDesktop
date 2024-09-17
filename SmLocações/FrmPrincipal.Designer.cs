fd6322c38fcd69b6b3b15049df40e80032e6b4de
EDUARDO RICHARD LEITE SANTOS <eduardo.rlsantos@senacsp.edu.br>
2024-09-17 09:15:13 -0300

    Commit teste

--- SmLocações/FrmPrincipal.Designer.cs
+++ SmLocações/FrmPrincipal.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1084, 623);
             Name = "FrmPrincipal";
             Text = "FrmPrincipal";
             Load += FrmPrincipal_Load;
--- SmLocaçõesLib/Banco.cs
+++ SmLocaçõesLib/Banco.cs
@@ -9,5 +9,5 @@ namespace SmLocaçõesLib
 {
     public class Banco
     {
-    }
+    } 
 }
