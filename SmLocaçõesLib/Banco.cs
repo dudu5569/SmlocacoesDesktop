2daece49c006e685540060dc315e8442620cac31
Duh <wakaw5302@gmail.com>
2024-10-14 11:17:33 -0300

    classe banco

--- SmLocaçõesLib/Banco.cs
+++ SmLocaçõesLib/Banco.cs
@@ -9,5 +9,19 @@ namespace SmLocaçõesLib
 {
     public class Banco
     {
-    } 
+        public static MySqlCommand Abrir()
+        {
+            string strconn = @"server=127.0.0.1;database=smlocaoesdb;user=root;password;";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            MySqlConnection cmd = new();
+
+            try
+            {
+                cn.Open();
+                
+            }
+
+            return cmd;
+        }
+    
 }
